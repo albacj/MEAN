12b85db074d4bf8f1767c9555e8406d2c18fb499
Alba Carmona <albacarmona91@gmail.com>
2017-11-22 13:43:39 +0100

    Importados los componentes Post y Posts.

--- mi-proyecto-angular/src/app/posts/posts.module.ts
+++ mi-proyecto-angular/src/app/posts/posts.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts.component';
+import { PostComponent } from '../post/post.component';
 
 @NgModule({
   imports: [
-    CommonModule
-  ],
-  declarations: []
+    CommonModule,
+    RouterModule.forRoot([{
+      path: "posts",
+      component: PostsComponent
+    }, {
+      path: "posts/:id",
+      component: PostComponent
+    }])
+        ],
+  declarations: [
+    PostsComponent,
+    PostComponent
+  ]
 })
 export class PostsModule { }
