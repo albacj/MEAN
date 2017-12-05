419e1ce2be134e4ac39eeffd41b3a74aab610a03
Alba Carmona <albacarmona91@gmail.com>
2017-12-05 13:20:03 +0100

    Creado el archivo /src/app/i18n-providers.ts

---
+++ mi-proyecto-angular/src/app/i18n-providers.ts
@@ -0,0 +1,19 @@
+import { TRANSLATIONS, TRANSLATIONS_FORMAT, LOCALE_ID } from '@angular/core';
+export function getTranslationProviders(): Promise<Object[]> {
+  const locale = document['locale'] as string;
+  const noProviders: Object[] = [];
+  if (!locale || locale === 'en-US') {
+    return Promise.resolve(noProviders);
+  }
+  return getTranslationsWithES6Import(locale)
+  .then((translations: string ) => [
+    { provide: TRANSLATIONS, useValue: translations },
+    { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' },
+    { provide: LOCALE_ID, useValue: locale }
+  ])
+  .catch(() => noProviders);
+}
+declare var System: any;
+function getTranslationsWithES6Import(locale: string) {
+  return System.import('../locale/messages.' + locale + '.xlf');
+}
